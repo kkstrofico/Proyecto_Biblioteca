49033e22df0eb40511978190bab648ff17e64623
Edier <guerraedier197@gmail.com>
2024-10-16 22:57:40 +0200

    Agregar funcionalidad de registrar Administrador -En el modulo registro usuario se agrego la clase Usuario -En el modulo registro usuario se agrego la clase Administrador -En la clase Administrador se agrego el metodo registrar(Que permiete registrar  en una lista los datos del administrador

--- src/registroUsuario.py
+++ src/registroUsuario.py
@@ -1,2 +1,9 @@
-def registrarse():
-    
+import bsd 
+class Usuario:
+    def __init__(self,nombre_usuario,contraseña):
+        self.nombre_usuario = nombre_usuario
+        self.contreseña = contraseña
+
+class Administrador(Usuario):
+    def registrar(self):
+        bsd.bibliotecarios.append({'Nombre':self.nombre_usuario,'Contraseña':self.contreseña})
